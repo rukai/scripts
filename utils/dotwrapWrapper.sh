1bf88dc881508aaf3878e1f4a747b92861da1447
Rukai <rubickent@gmail.com>
2014-02-18 22:08:37 +1100

    Forget stuff

--- utils/dotwrapWrapper.sh
+++ utils/dotwrapWrapper.sh
@@ -1,5 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+#Syntax: dotwrap file [engine]
+#standard dot engine used when not specified
 
 outputFile=$(echo "$1" | sed "s/\.dot/.png/")
 
-~/Scripts/utils/dotwrap.rb $1 | dot -Tpng > $outputFile
+if [[ -z $2 ]]
+then
+    type=dot
+else
+    type="$2"
+fi
+
+~/Scripts/utils/dotwrap.rb $1 | $type -Tpng > $outputFile
