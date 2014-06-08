12096c599fd879e25f0d308ade8e1524d80ba11a
Rukai <rubickent@gmail.com>
2014-06-08 13:25:51 +1000

    Added script for making github themed html markdown pages.

---
+++ utils/githubMarkdownCSS.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+{
+    echo "<link rel='stylesheet' href='file:///home/rubic/.githubMarkdown.css'/>"
+    echo "<script type='text/javascript' src='file:///home/rubic/Bin/latexit.js'></script>"
+    echo "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>"
+    pandoc $1
+} > `echo $1 | sed s/.markdown/.html/ | sed s/.md/.html/`
