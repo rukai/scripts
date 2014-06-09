cdade656890b9a12ac97f5189c1a8a583d155d39
Rukai <rubickent@gmail.com>
2014-06-09 13:26:44 +1000

    Added LaTeXMathML to the markdown scripts.

--- utils/githubMarkdownCSS.sh
+++ utils/githubMarkdownCSS.sh
@@ -2,7 +2,7 @@
 
 {
     echo "<link rel='stylesheet' href='file:///home/rubic/.githubMarkdown.css'/>"
-    echo "<script type='text/javascript' src='file:///home/rubic/Bin/latexit.js'></script>"
+    echo "<script type='text/javascript' src='file:///home/rubic/Scripts/utils/LaTeXMathML.js'></script>"
     echo "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>"
-    pandoc $1
+    pandoc --latexmathml $1
 } > `echo $1 | sed s/.markdown/.html/ | sed s/.md/.html/`
--- utils/markdownCSS.sh
+++ utils/markdownCSS.sh
@@ -2,7 +2,8 @@
 
 {
     echo "<link rel='stylesheet' href='file:///home/rubic/.markdown.css'/>"
-    echo "<script type='text/javascript' src='file:///home/rubic/Bin/latexit.js'></script>"
+    echo "<script type='text/javascript' src='file:///home/rubic/Bin/LaTeXMathML.js'></script>"
+    echo "<script type='text/javascript' src='file:///home/rubic/Scripts/utils/LaTeXMathML.js'></script>"
     echo "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>"
-    pandoc $1
+    pandoc --latexmathml $1
 } > `echo $1 | sed s/.markdown/.html/ | sed s/.md/.html/`
