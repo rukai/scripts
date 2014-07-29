eeeb4ccfe4878d2056ff775304070b8ffa6caec6
rukai <rubickent@gmail.com>
2014-07-29 20:04:12 +1000

    Added support for older formats to the multiplechoice script

--- utils/hscMultipleChoicer.py
+++ utils/hscMultipleChoicer.py
@@ -21,17 +21,27 @@ def multipleChoicer():
     marksFile = sys.argv[1]
     pdf = subprocess.check_output(["/usr/bin/env", "pdftotext", marksFile, "-"]).decode().splitlines()
 
+    questionCount=0
     #determine list of answers
-    if "Answer" not in pdf:
+    if "Answer" in pdf: #new format (single column)
+        offset = pdf.index("Answer")+1
+        questionCount=int(pdf[offset-3])
+        answers = pdf[offset:offset+questionCount]
+    elif "response" in pdf: # old format (multiple colums)
+        segmentLength = 10
+        offset = 0
+        answers = []
+        while "response" in pdf[offset:]:
+            offset = pdf.index("response", offset)+1
+            answers.extend(pdf[offset:offset+segmentLength])
+        questionCount=len(answers)
+    else:
         print("The passed file is not a valid/official set of answers to an HSC exam.")
         return
-    offset = pdf.index("Answer")+1
-    questionCount=int(pdf[offset-3])
-    answers = pdf[offset:offset+questionCount]
     
     i = 0 #question number
     #Display prompt once, then display question info and another prompt until past Q20
-    while i <= 20:
+    while i <= len(answers):
         if i != 0:
             print("{}) {}".format(i, answers[i-1]))
         if interactive:
