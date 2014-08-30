bace919eb313b150e1d303225402391a54c46561
rukai <rubickent@gmail.com>
2014-08-30 12:19:42 +1000

    Laptop syncing scripts now give the user a chance to confirm changes.

--- sync/pull.sh
+++ sync/pull.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-rsync --delete --files-from '/home/rubic/Scripts/sync/include.txt' -e ssh -ravuh rubic@xps.lan:/home/rubic/ /home/rubic
+sync="rsync --delete --files-from /home/rubic/Scripts/sync/include.txt -e ssh -ravuh rubic@xps.lan:/home/rubic/ /home/rubic"
+
+# Confirm files to be pulled
+$sync --dry-run
+read -p "Confirm sync? [y/n] " confirm
+
+# Run actual sync
+if [ "$confirm" == "y" ]
+then
+    $sync
+fi
--- sync/push.sh
+++ sync/push.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-rsync --delete --files-from '/home/rubic/Scripts/sync/include.txt' -e ssh -ravuh /home/rubic/ rubic@xps.lan:/home/rubic/
+sync="rsync --delete --files-from /home/rubic/Scripts/sync/include.txt -e ssh -ravuh /home/rubic/ rubic@xps.lan:/home/rubic/"
+
+# Confirm files to be pulled
+$sync --dry-run
+read -p "Confirm sync? [y/n] " confirm
+
+# Run actual sync
+if [ "$confirm" == "y" ]
+then
+    $sync
+fi
