fd743b6e90b43e2874154615ca5397678382956b
rukai <rubickent@gmail.com>
2014-09-28 14:46:32 +1000

    Backup script now backsup git folders but ignores ~/Video. It now asks for confirmation on what will be done before performing the backup

--- backup/runBackup.sh
+++ backup/runBackup.sh
@@ -7,4 +7,14 @@ pacman -Qqen > pacmanList.txt
 
 #backup to remote server
 cd ..
-rsync -avuh --delete --delete-excluded --exclude-from '/home/rubic/Scripts/backup/exclude.txt' -e ssh /home/rubic/ rubic@192.168.1.7:/home/rubic/XPS-Backup/
+sync="rsync -avuh --delete --delete-excluded --exclude-from /home/rubic/Scripts/backup/exclude.txt -e ssh /home/rubic/ rubic@192.168.1.7:/home/rubic/XPS-Backup/"
+
+# Confirm files to be pushed
+$sync --dry-run
+read -p "Confirm sync? [y/n] " confirm
+
+# Run actual sync
+if [ "$confirm" == "y" ]
+then
+    $sync
+fi
