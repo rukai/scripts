2c5bf8ad210e2b2748af89e0fbc9ad976b486f9a
rukai <rubickent@gmail.com>
2015-02-20 00:34:43 +1100

    Monitor setup scripts refactored

---
+++ monitor/desktop/landscape.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-0 --auto --rotate normal --output VGA-1 --auto --right-of DVI-0 --rotate normal
---
+++ monitor/desktop/portrait.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-0 --auto --rotate left --output VGA-1 --auto --right-of DVI-0 --rotate normal
